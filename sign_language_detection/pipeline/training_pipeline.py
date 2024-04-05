8e3af3120eec37949edf6cc78d53c8052375a943
Ayush Gandhi <ayushgandhi904@gmail.com>
2024-04-05 05:53:01 +0530

    data_validation completed

--- sign_language_detection/pipeline/training_pipeline.py
+++ sign_language_detection/pipeline/training_pipeline.py
@@ -2,14 +2,15 @@ import sys, os
 from sign_language_detection.logger import logging
 from sign_language_detection.exception import SignException
 from sign_language_detection.components.data_ingestion import DataIngestion
+from sign_language_detection.components.data_validation import DataValidation
 
-from sign_language_detection.entity.config_entity import DataIngestionConfig
-from sign_language_detection.entity.artifacts_entity import DataIngestionArtifact
+from sign_language_detection.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from sign_language_detection.entity.artifacts_entity import DataIngestionArtifact, DataValidationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        # self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
         # self.model_trainer_config = ModelTrainerConfig()
         # self.model_pusher_config = ModelPusherConfig()
         # self.s3_operations = S3Operation()
@@ -37,3 +38,29 @@ class TrainPipeline:
 
         except Exception as e:
             raise SignException(e, sys)
+        
+    
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SignException(e, sys) from e
+        
